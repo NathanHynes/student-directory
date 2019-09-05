471fc8e26c92d0ad70c57e41acbada2459f0f82d
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-05 17:04:19 +0100

    Deleted original students Array

--- directory.rb
+++ directory.rb
@@ -17,22 +17,6 @@ def input_students
   students
 end
 
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :september},
-  {name: "Darth Vader", cohort: :september},
-  {name: "Nurse Ratched", cohort: :september},
-  {name: "Michael Corleone", cohort: :september},
-  {name: "Alex DeLarge", cohort: :september},
-  {name: "The Wicked Witch of the West", cohort: :september},
-  {name: "Terminator", cohort: :september},
-  {name: "Freddy Krueger", cohort: :september},
-  {name: "The Joker", cohort: :september},
-  {name: "Joffrey Baratheon", cohort: :september},
-  {name: "Norman Bates", cohort: :september}
-]
-=end
-
 def print_header
   puts "The Students of Villains Academy"
   puts "-------------"
