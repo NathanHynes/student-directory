562dda61ad77bdffae0860e15aac2870149ccd08
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-06 11:59:55 +0100

    Completed sixth exercise

---
+++ Exercises/directory6.rb
@@ -0,0 +1,39 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+    students << {name: name, cohort: :september}
+    puts "Now we have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end
+
+def print_header
+  puts "The Students of Villains Academy".center(50)
+  puts "-------------".center(50)
+end
+
+def print(students)
+  students.each_with_index do |student,index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students".center(50)
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
